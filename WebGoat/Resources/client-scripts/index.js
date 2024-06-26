1e8f9e89ef3daa3e8274c75a311fbdb2ed6bef6e
1Bira <ubiratan.gleidson@gmail.com>
2024-06-26 14:48:25 +0100

    add env

--- WebGoat/Resources/client-scripts/index.js
+++ WebGoat/Resources/client-scripts/index.js
@@ -2,11 +2,12 @@
 
 // Exemplo de um segredo exposto no código
 const apiKey = '12345-ABCDE-SECRET-KEY';
+const apiKey2 = process.env.API_KEY;
 
 // Função que utiliza o segredo
 function fetchData() {
   // Simulação de uso do segredo (não faça isso na prática!)
-  console.log(`Fetching data using API Key: ${apiKey}`);
+  console.log(`Fetching data using API Key: ${apiKey} e ${apiKey2}`);
 }
 
 // Chamada da função fetchData

8ce00d12c3db22be7aadbf7088a5f8c1a43f018e
1Bira <ubiratan.gleidson@gmail.com>
2024-06-26 14:55:35 +0100

    token

--- WebGoat/Resources/client-scripts/index.js
+++ WebGoat/Resources/client-scripts/index.js
@@ -2,6 +2,7 @@
 
 // Exemplo de um segredo exposto no código
 const github_personal_access_token = '12345-ABCDE-SECRET-KEY';
+const azure_active_directory_application_secret = '12345-ABCDE-SECRET-KEY-123';
 const apiKey2 = process.env.API_KEY;
 
 // Função que utiliza o segredo
